b0aa2d2fd678e72d0c05ac7aaec73fffe54bf3a5
kennethvt437 <55422012+kennethvt437@users.noreply.github.com>
2019-09-20 14:27:43 -0400

    Added getter methods

--- ComplexNumber.java
+++ ComplexNumber.java
@@ -32,6 +32,78 @@ public class Complex()
          this.b = b;
      }
     
+    /**
+     * returns the real part of the number
+     * @return the real part of the number
+     */
+    public double real()
+    {
+        return a;
+    }
+    
+    /**
+     * returns the real part of the number
+     * @return the real part of the number
+     */
+    public Complex realAsComplex()
+    {
+        return new Complex(a, 0);
+    }
+    
+    /**
+     * returns the imaginary part of the number
+     * @return the imaginary part of the number
+     */
+    public double imag()
+    {
+        return b;
+    }
+    
+    /**
+     * returns the imaginary part of the number
+     * @return the imaginary part of the number
+     * output has an imaginary part of zero and 
+     * a real part of b
+     */
+    public Complex imagAsComplex()
+    {
+        return new Complex(b, 0);
+    }
+    
+    /**
+     * Represents the complex number as a String
+     * of the form a + bi
+     *
+     * @return a String representation of the number
+     * @Override
+     */
+    public String toString()
+    {
+        if (b > 0)
+        {
+            return a + " + " + b + "i";
+        }
+        else if (b < 0)
+        {
+            return a + " - " + (-1) * b + "i";
+        }
+        else
+        {
+            return "" + a;
+        }
+    }
+    
+    /**
+     * Tests if two complex numbers are equal
+     * @return true if both real and imaginary
+     * parts match, false otherwise
+     */
+    public boolean equals(complex2)
+    {
+        //need to implement
+        return false;
+    }
+    
     /**,
      * Adds two complex numbers
      * 
